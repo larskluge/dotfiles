132cfb18a9ef8312c53b9455e793a363d4cafcc6
aekym <me@aekym.com>
2011-12-23 15:17:44 -0500

    use new jslint bin for js syntax check

--- .vim/bin/jslint.rb
+++ .vim/bin/jslint.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
 
-system("v8 -e \"var files = [#{ARGV.map { |v| "'#{v}'" }.join(',')}]\" #{File.dirname(__FILE__)}/jslint.js")
+# system("v8 -e \"var files = [#{ARGV.map { |v| "'#{v}'" }.join(',')}]\" #{File.dirname(__FILE__)}/jslint.js")
+system("jslint #{ARGV.map { |v| "'#{v}'" }.join(' ')}")
 
