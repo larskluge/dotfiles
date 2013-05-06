9ff2bfd6bba8d5151f563cacd78557df346b23f5
aekym <me@aekym.com>
2013-05-06 18:04:20 -0400

    remove all custom plugins in preparation for using vundle

--- .vim/bin/jslint.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby
-
-# system("v8 -e \"var files = [#{ARGV.map { |v| "'#{v}'" }.join(',')}]\" #{File.dirname(__FILE__)}/jslint.js")
-system("jslint #{ARGV.map { |v| "'#{v}'" }.join(' ')}")
-
